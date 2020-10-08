3b4b22ea7c3811ed14b669117b1c3c3e50a8aeec
NtwaliHeritier <heritierntwali@gmail.com>
2020-10-08 14:31:34 +0200

    update database.yml to include postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_10_08_105818) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cars", force: :cascade do |t|
     t.string "make"
     t.integer "year"
